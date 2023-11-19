50c0e3a6e3280f69c65ceb11a9fab41f1d3c639f
Kholifatul Ardi <2tkj2.kholifatul@gmail.com>
2023-11-19 23:03:40 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+// require('dotenv').config()
 import Layout from "./components/layout/Layout";
-
+// import 'dotenv/config'
 function App() {
+  // dotenv.config();
+  
   return <Layout />;
 }
 
